31f2e43bac32210a3b3b80d6e2de6bae4d3d2cc6
Sujitha Wijewantha <sujithacrysis@gmail.com>
2022-03-04 08:07:04 +0530

    wish list and auth done

--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -7,5 +7,17 @@
 const { createCoreService } = require("@strapi/strapi").factories;
 
 module.exports = createCoreService("api::game.game", ({ strapi }) => ({
-    
+  async create(ctx) {
+    const { data } = ctx;
+    const existingGame = await strapi.entityService.findMany("api::game.game", {
+      filters: { api_id: data.api_id },
+    });
+    if (existingGame.length) {
+      return existingGame[0];
+    }
+    const newGame = await strapi.entityService.create("api::game.game", {
+      data,
+    });
+    return newGame;
+  },
 }));
--- src/api/user-games-list/controllers/user-games-list.js
+++ src/api/user-games-list/controllers/user-games-list.js
@@ -5,22 +5,59 @@
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
-
-const setList = (ctx, type) => {
-  const { body } = ctx;
+const setList = async (ctx, type) => {
+    
+}
+const addToList = async (ctx, type) => {
+  const data = ctx.request.body;
   const { user } = ctx.state;
-  body.listType = type;
-  body.user = user;
-  ctx.body = body;
 
-
+  const game = await strapi.service("api::game.game").create({
+    data: data.game,
+  });
+  const existingWishlist = await strapi.entityService.findMany(
+    "api::user-games-list.user-games-list",
+    {
+      filters: { game: game.id, user: user.id, list_type: type },
+    }
+  );
+  if (existingWishlist.length) {
+    return existingWishlist[0];
+  }
+  return await strapi.entityService.create(
+    "api::user-games-list.user-games-list",
+    {
+      data: {
+        game: game.id,
+        user: user.id,
+        platform: data.platform,
+        list_type: type,
+      },
+    }
+  );
 };
 
 module.exports = createCoreController(
   "api::user-games-list.user-games-list",
   ({ strapi }) => ({
-    async addToGameLibrary(ctx) {},
-    async addToWishList(ctx) {},
+    async addToGameLibrary(ctx) {
+      try {
+        const body = await addToList(ctx, "LIBRARY");
+        ctx.body = body;
+      } catch (error) {
+        console.log(error);
+        ctx.badRequest("Invalid Platform id or Genre", error);
+      }
+    },
+    async addToWishList(ctx) {
+      try {
+        const body = await addToList(ctx, "WISHLIST");
+        ctx.body = body;
+      } catch (error) {
+        console.log(error);
+        ctx.badRequest("Invalid Platform id or Genre", error);
+      }
+    },
     async setGameLibrary(ctx) {},
     async setWishList(ctx) {},
   })
